d2199631a78c8b00e077d38152b810f9d64618c7
ray-curran <rpcurr@gmail.com>
2016-05-01 13:26:17 -0500

    Basic repo webview working

---
+++ App/Components/Helpers/WebView.js
@@ -0,0 +1,31 @@
+var React = require('react-native');
+
+var {
+  View,
+  WebView,
+  StyleSheet
+} = React;
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F6F6EF',
+    flexDirection: 'column'
+  }
+});
+
+class Web_View extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <WebView source={{uri: this.props.url}} />
+      </View>
+      )
+  }
+}
+
+Web_View.propTypes= {
+  url: React.PropTypes.string.isRequired
+}
+
+module.exports = Web_View;
